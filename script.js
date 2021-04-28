8f91193400c6ac0ca1782e0c072035e40a03de29
Brian <brianjmorrissey@gmail.com>
2021-04-28 07:45:49 -0500

    Traversy Media tutorial on scrolling animation

---
+++ script.js
@@ -0,0 +1,24 @@
+const boxes = document.querySelectorAll('.box');
+
+
+
+
+const checkBoxes = () => {
+    const triggerBottom = window.innerHeight / 7 * 4;
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show');
+        }
+    })
+}
+
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes();
+
